2ff9013cbdebe9fc47d40182885fe91dbac65496
Zhen Lu <user@Zhens-MacBook-Pro.local>
2014-08-12 12:15:14 -0700

    First version with tmp ui.

--- app/src/main/java/com/left/to/zlu/identifying/IdentifyingActivity.java
+++ app/src/main/java/com/left/to/zlu/identifying/IdentifyingActivity.java
@@ -1,17 +1,71 @@
 package com.left.to.zlu.identifying;
 
 import android.app.Activity;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.media.ExifInterface;
+import android.media.Image;
+import android.net.Uri;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import java.io.File;
+import java.io.IOException;
 
 
 public class IdentifyingActivity extends Activity {
+
+    private static String TAG = "IdentifyingActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_identifying);
+
+        LinearLayout blurLayout = (LinearLayout) findViewById(R.id.blur_background);
+
+        long imageId = 57L;
+        Uri imageUri = Uri.withAppendedPath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "" + imageId);
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MediaStore.Images.Media.DESCRIPTION, "Some description ABC_XYZ");
+        getContentResolver().update(imageUri, contentValues, null, null);
+        String desc = getDescriptionFromURI(imageUri);
+        Log.d(TAG, "Image description: " + desc);
+
+        String pathName = Environment.getExternalStorageDirectory().getPath() + "/DCIM/.thumbnails/" + "1407794490424.jpg";
+        Bitmap bitmap = BitmapFactory.decodeFile(pathName);
+        BitmapDrawable backgroundDrawable = new BitmapDrawable(getResources(), bitmap);
+        try {
+            ExifInterface exifInterface = new ExifInterface(pathName);
+            final String comment = exifInterface.getAttribute("UserComment");
+            final String description = exifInterface.getAttribute("ImageDescription");
+            exifInterface.setAttribute("ImageDescription", "Something New");
+            exifInterface.setAttribute("UserComment", "does comment work?");
+            exifInterface.saveAttributes();
+            if (description != null) {
+                Log.d(TAG, description);
+            }
+            if (comment != null) {
+                Log.d(TAG, comment);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.JELLY_BEAN) {
+            blurLayout.setBackgroundDrawable(backgroundDrawable);
+        } else {
+            blurLayout.setBackground(backgroundDrawable);
+        }
     }
 
 
@@ -32,5 +86,13 @@ public class IdentifyingActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public String getDescriptionFromURI(Uri uri) {
+        Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+        cursor.moveToFirst();
+        int idx = cursor.getColumnIndex(MediaStore.Images.Media.DESCRIPTION);
+        String description = cursor.getString(idx);
+        return description;
     }
 }
